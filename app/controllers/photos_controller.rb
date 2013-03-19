45b49a3f11f04359f1c94a413516412ce5da2253
Yu Jiang Tham <ytham@apple.com>
2013-03-19 13:20:36 -0700

    Got Photos Working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,36 @@
 class PhotosController < ApplicationController
 
+  def new
+    @photo = Photo.new
+    respond_to do |format|
+      format.html
+      format.json { render json: @photo }
+    end
+  end
+
   def show
-    @photo = Photo.find_by_id(params[:id])
+    @photo = Photo.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @photo }
+    end
   end
 
   def index
-    @photos = Photos.all
+    @photos = Photo.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @photos }
+    end
   end
-
+  
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
 end
+
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,11 @@
 module PhotosHelper
+
+  def remove_underscores(name)
+    no_underscores = name.gsub("_", " ")
+  end
+
+  def add_extension(name)
+    name_with_extension = name + ".jpg"
+  end
+
 end
