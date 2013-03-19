adfb2d1344ea453ffb13510656cc2ede48edd3b0
Yu Jiang Tham <ytham@apple.com>
2013-03-18 20:34:18 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogallery::Application.config.secret_token = '2519033a109ab6d8effcf2c38cf5a8482de6660ff57cee14b636528b1a06d6821cc25faa9901e063296b6ac234e38e9082fe72801b3f166c6ba3a895a66ec665'
