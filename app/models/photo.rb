bacfe98393a3e30085dc18852603020da86125d3
Yu Jiang Tham <ytham@apple.com>
2013-03-20 21:18:40 -0700

    CSS sidebars

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,12 @@
 class Photo < ActiveRecord::Base
   attr_accessible :caption, :name
 
+  def next
+    Photo.first(conditions: ["id > ?", id], order: "id asc")
+  end
+
+  def previous
+    Photo.first(conditions: ["id < ?", id], order: "id desc")
+  end
 
 end
