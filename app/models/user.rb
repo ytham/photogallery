02daf96ef847eb5282720bdfd18f24daaed24b6e
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-02 22:12:12 -0700

    Final changes, then push to production

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,10 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email, on: :create, message: "is already taken."
 
-  #validate correct email
   validates :name, length: { in: 3..25 }
   validates :password, length: { in: 6..96 }, unless: :update
+
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 
   def self.authenticate(email, password)
     user = find_by_email(email.downcase)
