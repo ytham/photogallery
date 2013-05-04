a25343e7150430f83166fd5f2288abb9baff902e
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-04 08:41:31 -0700

    Testing removing attr_accessible :avatar for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation, :avatar
+  attr_accessible :name, :email, :password, :password_confirmation #, :avatar
   attr_accessor :password
 
   has_many :photos, dependent: :destroy
